d37c4d8cceac8d2e00af5e96368b6727f7207460
lbcanfield <lewie.b.canfield@gmail.com>
2022-11-26 02:43:36 -0600

    App.js -imported axios - acquired data inside the componentDidMount( ) - created a variable for server errors - used catch to set the error message.

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+// JavaScript Library Imports
+import React from 'react';
+import axios from 'axios';
+
+// Class/Component Imports
 import TodoList from '../components/TodoList';
 import Form from '../components/Form';
 
 const URL = 'http://localhost:9000/api/todos'
+const error_msg = '';
 
 export default class App extends React.Component {
   constructor() {
@@ -11,14 +16,31 @@ export default class App extends React.Component {
       tasks: []
     }
   }
+
+  componentDidMount() {
+    console.log('Component Did Mount');
+    axios.get(URL)
+      .then(response => {
+        // console.log(response.data.data);
+        this.setState({ ...this.state, tasks: response.data.data })
+      })
+      .catch(error => {
+        if (error !== null) {
+          error_msg = error
+        }
+        // console.error(error)
+      })
+  }
+
+
   render() {
     console.log('Component Did Render to the Browser')
     return (
       <div>
-        <div id='error'>ERROR MESSAGE</div>    {/*id (error) found in styles.css*/}
-        <TodoList />
+        <div id='error'>{error_msg}</div>    {/*id (error) found in styles.css*/}
+        < TodoList />
         <Form />
-      </div>
+      </div >
     )
   }
 }
